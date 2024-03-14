6057f1677620d446e3e9ce1c52afb58394243beb
Altes10 <163399107+Altes10@users.noreply.github.com>
2024-03-14 11:59:33 +0100

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import VueAxios from 'vue-axios'
-import http from "./http"
-
-createApp(App).use(router).use(VueAxios, http).mount('#app')
